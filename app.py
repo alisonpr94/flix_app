4ac8c12b397089f7f5d8d147f49b7d244202f877
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2024-04-07 15:08:07 -0300

    page de login

--- app.py
+++ app.py
@@ -3,30 +3,34 @@ from genres.page import show_genres
 from actors.page import show_actors
 from movies.page import show_movies
 from reviews.page import show_reviews
+from login.page import show_login
 
 
 def main():
-    st.title('Flix App')
+    if 'token' not in st.session_state:
+        show_login()
+    else:
+        st.title('Flix App')
 
-    menu_option = st.sidebar.selectbox(
-        'Selecione uma opção',
-        ['Início', 'Gêneros', 'Atores', 'Filmes', 'Avaliações']
-    )
+        menu_option = st.sidebar.selectbox(
+            'Selecione uma opção',
+            ['Início', 'Gêneros', 'Atores', 'Filmes', 'Avaliações']
+        )
 
-    if menu_option == 'Início':
-        st.write('Início')
+        if menu_option == 'Início':
+            st.write('Início')
 
-    if menu_option == 'Gêneros':
-        show_genres()
+        if menu_option == 'Gêneros':
+            show_genres()
 
-    if menu_option == 'Atores':
-        show_actors()
+        if menu_option == 'Atores':
+            show_actors()
 
-    if menu_option == 'Filmes':
-        show_movies()
-    
-    if menu_option == 'Avaliações':
-        show_reviews()
+        if menu_option == 'Filmes':
+            show_movies()
+        
+        if menu_option == 'Avaliações':
+            show_reviews()
 
 
 if __name__ == '__main__':
